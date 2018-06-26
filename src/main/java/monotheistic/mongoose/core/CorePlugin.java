d3a05aa44b7a07e461bdd069e2df2741e19fd665
BuildTools <unconfigured@null.spigotmc.org>
2018-06-26 11:47:18 -0400

    Added a bit of static abuse.

--- src/main/java/monotheistic/mongoose/core/CorePlugin.java
+++ src/main/java/monotheistic/mongoose/core/CorePlugin.java
@@ -5,6 +5,7 @@ import monotheistic.mongoose.core.components.playerdata.database.Database;
 import monotheistic.mongoose.core.files.Configuration;
 import monotheistic.mongoose.core.gui.GUIListener;
 import monotheistic.mongoose.core.strings.PluginStrings;
+import monotheistic.mongoose.core.utils.ScheduleUtils;
 import org.bukkit.ChatColor;
 
 public abstract class CorePlugin extends ComponentPlugin {
@@ -12,6 +13,8 @@ public abstract class CorePlugin extends ComponentPlugin {
     public CorePlugin() {
         setup();
         onDisable(PluginStrings::nullifyEverything);
+        onEnable(() -> ScheduleUtils.set(this));
+        onDisable(() -> ScheduleUtils.set(null));
         addChild(new GUIListener());
     }
 
--- src/main/java/monotheistic/mongoose/core/utils/ScheduleUtils.java
+++ src/main/java/monotheistic/mongoose/core/utils/ScheduleUtils.java
@@ -4,8 +4,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
-public interface ScheduleUtils {
-    static void sync(JavaPlugin main, Runnable runnable) {
+public class ScheduleUtils {
+    private static JavaPlugin plugin;
+
+    public static void set(JavaPlugin plugin) {
+        ScheduleUtils.plugin = plugin;
+    }
+
+    public static void sync(JavaPlugin main, Runnable runnable) {
         Bukkit.getScheduler().runTask(main, runnable);
     }
 
@@ -13,19 +19,19 @@ public interface ScheduleUtils {
         return Bukkit.getScheduler().runTaskAsynchronously(main, runnable);
     }
 
-    static void repeatingSync(JavaPlugin main, Runnable runnable, long everyTicks, boolean startingNow) {
+    public static void repeatingSync(JavaPlugin main, Runnable runnable, long everyTicks, boolean startingNow) {
         Bukkit.getScheduler().runTaskTimer(main, runnable, startingNow ? 0 : everyTicks, everyTicks);
     }
 
-    static void repeatingAsync(JavaPlugin main, Runnable runnable, long everyTicks, boolean startingNow) {
+    public static void repeatingAsync(JavaPlugin main, Runnable runnable, long everyTicks, boolean startingNow) {
         Bukkit.getScheduler().runTaskTimerAsynchronously(main, runnable, startingNow ? 0 : everyTicks, everyTicks);
     }
 
-    static void laterSync(JavaPlugin main, Runnable runnable, long delay) {
+    public static void laterSync(JavaPlugin main, Runnable runnable, long delay) {
         Bukkit.getScheduler().runTaskLater(main, runnable, delay);
     }
 
-    static void laterAsync(JavaPlugin main, Runnable runnable, long delay) {
+    public static void laterAsync(JavaPlugin main, Runnable runnable, long delay) {
         Bukkit.getScheduler().runTaskLaterAsynchronously(main, runnable, delay);
     }
 }
