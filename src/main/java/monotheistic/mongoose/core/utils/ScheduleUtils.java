9af139f3be4aebebedc0c12a838cf274c59add58
BuildTools <unconfigured@null.spigotmc.org>
2018-06-26 11:49:55 -0400

    Added a bit of static abuse.

--- src/main/java/monotheistic/mongoose/core/utils/ScheduleUtils.java
+++ src/main/java/monotheistic/mongoose/core/utils/ScheduleUtils.java
@@ -5,33 +5,33 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
 public class ScheduleUtils {
-    private static JavaPlugin plugin;
+    private static JavaPlugin main;
 
     public static void set(JavaPlugin plugin) {
-        ScheduleUtils.plugin = plugin;
+        ScheduleUtils.main = plugin;
     }
 
-    public static void sync(JavaPlugin main, Runnable runnable) {
+    public static void sync(Runnable runnable) {
         Bukkit.getScheduler().runTask(main, runnable);
     }
 
-    static BukkitTask async(JavaPlugin main, Runnable runnable) {
+    static BukkitTask async(Runnable runnable) {
         return Bukkit.getScheduler().runTaskAsynchronously(main, runnable);
     }
 
-    public static void repeatingSync(JavaPlugin main, Runnable runnable, long everyTicks, boolean startingNow) {
+    public static void repeatingSync(Runnable runnable, long everyTicks, boolean startingNow) {
         Bukkit.getScheduler().runTaskTimer(main, runnable, startingNow ? 0 : everyTicks, everyTicks);
     }
 
-    public static void repeatingAsync(JavaPlugin main, Runnable runnable, long everyTicks, boolean startingNow) {
+    public static void repeatingAsync(Runnable runnable, long everyTicks, boolean startingNow) {
         Bukkit.getScheduler().runTaskTimerAsynchronously(main, runnable, startingNow ? 0 : everyTicks, everyTicks);
     }
 
-    public static void laterSync(JavaPlugin main, Runnable runnable, long delay) {
+    public static void laterSync(Runnable runnable, long delay) {
         Bukkit.getScheduler().runTaskLater(main, runnable, delay);
     }
 
-    public static void laterAsync(JavaPlugin main, Runnable runnable, long delay) {
+    public static void laterAsync(Runnable runnable, long delay) {
         Bukkit.getScheduler().runTaskLaterAsynchronously(main, runnable, delay);
     }
 }
