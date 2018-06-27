e381ed548c8f7956fa1a8337746762ac36d56383
BuildTools <unconfigured@null.spigotmc.org>
2018-06-26 23:10:11 -0400

    Fixed GUI bug

--- src/main/java/monotheistic/mongoose/core/gui/GUIListener.java
+++ src/main/java/monotheistic/mongoose/core/gui/GUIListener.java
@@ -2,6 +2,7 @@ package monotheistic.mongoose.core.gui;
 
 import com.gitlab.avelyn.architecture.base.Component;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
@@ -12,22 +13,26 @@ import static com.gitlab.avelyn.core.base.Events.listen;
 public class GUIListener extends Component {
     public GUIListener() {
         addChild(listen((InventoryClickEvent event) -> {
-            if (event.getClickedInventory() == null)
+            final Inventory clicked = event.getClickedInventory();
+            if (clicked == null)
                 return;
-            final InventoryHolder holder = event.getClickedInventory().getHolder();
+            final InventoryHolder holder = event.getView().getTopInventory().getHolder();
             if (holder instanceof MyGUI) {
-                event.setCancelled(true);
-                final MyGUI gui = (MyGUI) holder;
-                final ItemStack currentItem = event.getCurrentItem();
-                if (currentItem == null)
-                    return;
-                gui.allTimeListeners.forEach(listener -> listener.accept(event));
-                final Consumer<InventoryClickEvent> action = gui.listeners.get(currentItem);
-                if (action != null) {
-                    action.accept(event);
+                if (clicked.getHolder() != null && clicked.getHolder() == holder) {
+                    event.setCancelled(true);
+                    final MyGUI gui = (MyGUI) holder;
+                    final ItemStack currentItem = event.getCurrentItem();
+                    if (currentItem == null)
+                        return;
+                    gui.allTimeListeners.forEach(listener -> listener.accept(event));
+                    final Consumer<InventoryClickEvent> action = gui.listeners.get(currentItem);
+                    if (action != null) {
+                        action.accept(event);
+                    }
                 }
             }
         }));
+
         onDisable(() -> {
             PaginatorGUI.EMERALD_FORWARDS = null;
             PaginatorGUI.REDSTONE_BACK = null;
