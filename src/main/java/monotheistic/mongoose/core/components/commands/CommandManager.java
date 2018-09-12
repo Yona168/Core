88297906a9cf697c61d596771b08dca013762d9c
BuildTools <unconfigured@null.spigotmc.org>
2018-09-11 22:36:45 -0400

    Meh

--- src/main/java/monotheistic/mongoose/core/components/commands/CommandManager.java
+++ src/main/java/monotheistic/mongoose/core/components/commands/CommandManager.java
@@ -25,7 +25,7 @@ public class CommandManager extends AbstractCommandManagerComponent {
         for (SubCommand subCommand : super.getCommands()) {
             if (subCommand.name().equalsIgnoreCase(strings[0]) || (subCommand.aliases().isPresent() && contains(subCommand.aliases().get(), strings[0]))) {
                 if (commandSender.hasPermission(subCommand.requiredPermissions(true)) || commandSender.isOp()) {
-                    return subCommand.onCommand(commandSender, Arrays.copyOfRange(strings, 1, strings.length));
+                    return subCommand.onCommand(commandSender, command, s, Arrays.copyOfRange(strings, 1, strings.length));
                 } else {
                     commandSender.sendMessage(PluginStrings.noPerms());
                     return false;
