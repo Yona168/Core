7a5141a157c49f558adef80814b4f55260418010
BuildTools <unconfigured@null.spigotmc.org>
2018-10-14 21:36:01 -0400

    Added permission support

--- src/main/java/monotheistic/mongoose/core/CorePlugin.java
+++ src/main/java/monotheistic/mongoose/core/CorePlugin.java
@@ -1,32 +1,10 @@
 package monotheistic.mongoose.core;
 
 import com.gitlab.avelyn.core.components.ComponentPlugin;
-import monotheistic.mongoose.core.components.commands.CommandSelector;
-import monotheistic.mongoose.core.components.commands.ExecutableCommand;
-import monotheistic.mongoose.core.components.commands.PluginInfo;
-import monotheistic.mongoose.core.components.commands.Test;
 import monotheistic.mongoose.core.gui.GUIListener;
-import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
-
-import java.util.List;
 
 public class CorePlugin extends ComponentPlugin {
     public CorePlugin() {
         addChild(new GUIListener());
-        PluginInfo info = new PluginInfo("CorePlugin", "CP", ChatColor.YELLOW, ChatColor.BLACK);
-        final CommandSelector selector = new CommandSelector(info, new ExecutableCommand() {
-            @Override
-            public boolean execute(CommandSender sender, String cmd, String[] args, PluginInfo pluginInfo, List<Object> objs) {
-                sender.sendMessage("BEEP");
-                return true;
-            }
-        });
-        selector.addChild(new Test());
-        addChild(selector);
-        onEnable(() ->
-                getCommand("tt").setExecutor(selector));
-
     }
-
 }
