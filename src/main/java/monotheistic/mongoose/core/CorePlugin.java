4cc0d7a9175ea08d9d5dd2fa2a6a2f2b27dc717b
BuildTools <unconfigured@null.spigotmc.org>
2018-12-17 21:16:42 -0500

    Had a duplicated dependency. Somehow.

--- src/main/java/monotheistic/mongoose/core/CorePlugin.java
+++ src/main/java/monotheistic/mongoose/core/CorePlugin.java
@@ -1,11 +1,16 @@
 package monotheistic.mongoose.core;
 
 import com.gitlab.avelyn.core.components.ComponentPlugin;
+import monotheistic.mongoose.core.files.Configuration;
 import monotheistic.mongoose.core.gui.GUIListener;
 
 public class CorePlugin extends ComponentPlugin {
   public CorePlugin() {
     addChild(new GUIListener());
+    onEnable(() -> {
+      Configuration config = Configuration.loadConfiguration(getDataFolder().toPath(), "config.yml");
+      System.out.println(config.get("test", String.class));
+    });
   }
 
 }
