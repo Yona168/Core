35d1bf07b3e0d81c56b99b7975816a7de601c3b5
Yona168 <38598309+Yona168@users.noreply.github.com>
2018-12-17 21:21:50 -0500

    Forgot to delete test things

--- src/main/java/monotheistic/mongoose/core/CorePlugin.java
+++ src/main/java/monotheistic/mongoose/core/CorePlugin.java
@@ -1,16 +1,11 @@
 package monotheistic.mongoose.core;
 
 import com.gitlab.avelyn.core.components.ComponentPlugin;
-import monotheistic.mongoose.core.files.Configuration;
 import monotheistic.mongoose.core.gui.GUIListener;
 
 public class CorePlugin extends ComponentPlugin {
   public CorePlugin() {
     addChild(new GUIListener());
-    onEnable(() -> {
-      Configuration config = Configuration.loadConfiguration(getDataFolder().toPath(), "config.yml");
-      System.out.println(config.get("test", String.class));
-    });
   }
 
 }
